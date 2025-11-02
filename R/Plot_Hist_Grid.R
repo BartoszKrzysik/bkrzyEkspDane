8782b81387e1fce8a9a3134d3e817e830e59bd25
Bartosz Krzysik <b.krzysik2@gmail.com>
2025-11-02 17:28:46 +0100

    hotfix

--- R/Plot_Hist_Grid.R
+++ R/Plot_Hist_Grid.R
@@ -12,11 +12,6 @@
 #' @examples
 #' plot_hist_grid(mtcars, factor_cols = c("cyl"))
 #'
-library(ggplot2)
-library(plotly)
-library(dplyr)
-library(ggthemes)
-
 plot_hist_grid <- function(df, factor_cols = NULL) {
   prep <- prepare_qualitative(df, factor_cols)
   df_local <- prep$df
@@ -29,34 +24,37 @@ plot_hist_grid <- function(df, factor_cols = NULL) {
   
   for (f in factor_cols) {
     for (n in numeric_cols) {
+      
       x_vals <- na.omit(df_local[[n]])
       unique_vals <- length(unique(x_vals))
       
-      if(unique_vals <= 30) {
+      if (unique_vals <= 30) {
         binwidth <- 1
       } else {
         iqr_val <- IQR(x_vals)
         n_obs <- length(x_vals)
         binwidth <- 2 * iqr_val / (n_obs)^(1/3)
-        if(binwidth == 0 || is.na(binwidth)) {
+        if (binwidth == 0 || is.na(binwidth)) {
           binwidth <- diff(range(x_vals)) / 30
         }
-        decimal_prec <- decimal_precision(x_vals)
-        binwidth <- signif(binwidth, decimal_prec)
+        dec <- decimal_precision(x_vals)
+        binwidth <- signif(binwidth, digits = dec + 1)
       }
       
-      df_bins <- ggplot_build(
-        ggplot(df_local, aes_string(x = n, fill = f)) +
-          geom_histogram(binwidth = binwidth)
-      )$data[[1]]
-      
-      df_bins <- df_bins %>%
+      df_bins <- df_local %>%
+        select(all_of(c(f, n))) %>%
+        drop_na() %>%
+        group_by(!!sym(f)) %>%
+        mutate(bin = cut(!!sym(n), breaks = seq(min(!!sym(n)), max(!!sym(n)) + binwidth, by = binwidth), right = FALSE)) %>%
+        group_by(!!sym(f), bin) %>%
+        summarise(count = n(), .groups = "drop") %>%
         mutate(
-          range = paste0("[", signif(xmin, 4), " – ", signif(xmax, 4), ")"),
-          text = paste0("Zakres: ", range, "<br>Ilość: ", count)
+          xmin = as.numeric(sub("\\[|,.*", "", bin)),
+          xmax = as.numeric(sub(".*,|\\)", "", bin)),
+          text = paste0("Zakres: [", xmin, " – ", xmax, ")<br>Ilość: ", count)
         )
       
-      p <- ggplot(df_bins, aes(x = x, y = count, fill = fill, text = text)) +
+      p <- ggplot(df_bins, aes(x = xmin, y = count, fill = !!sym(f), text = text)) +
         geom_col(color = "black", alpha = 0.7, width = binwidth) +
         facet_wrap(as.formula(paste("~", f)), scales = "free") +
         ggthemes::theme_clean(base_size = 15) +
@@ -69,4 +67,3 @@ plot_hist_grid <- function(df, factor_cols = NULL) {
   
   return(plots)
 }
-
